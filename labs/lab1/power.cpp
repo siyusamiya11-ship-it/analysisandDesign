bcf3b12aa21f6543c258924057dc8b9953d223c4
Samiya <siyusamiya11@gmail.com>
2026-01-23 11:30:16 +0530

    Power

---
+++ labs/lab1/power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int power(int a, int b) {
+    if (b == 0)
+        return 1;          
+    else
+        return a * power(a, b - 1);   
+}
+
+int main() {
+    int a, b;
+    cout << "Enter base: ";
+    cin >> a;
+    cout << "Enter power: ";
+    cin >> b;
+
+    cout << "Result = " << power(a, b);
+    return 0;
+}
