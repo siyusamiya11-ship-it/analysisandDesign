baa414fc763c5baec07887d06b687f33fc51cff6
Samiya <siyusamiya11@gmail.com>
2026-01-27 22:29:36 +0530

    consecutive number

---
+++ labs/lab1/consecutive.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Recursive XOR from 1 to n
+int xorRange(int n) {
+    if (n == 0)
+        return 0;
+    return n ^ xorRange(n - 1);
+}
+
+// Recursive XOR of array elements
+int xorArray(int arr[], int n) {
+    if (n == 0)
+        return 0;
+    return arr[n - 1] ^ xorArray(arr, n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    int arr[n - 1];
+    cout << "Enter " << n - 1 << " numbers:\n";
+
+    for (int i = 0; i < n - 1; i++)
+        cin >> arr[i];
+
+    int result = xorRange(n) ^ xorArray(arr, n - 1);
+
+    cout << "Missing number is: " << result;
+    return 0;
+}
