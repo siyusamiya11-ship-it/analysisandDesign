173a64a4477bd5c9c6c6bfcacccdcfa92bc836eb
Samiya <siyusamiya11@gmail.com>
2026-01-28 00:18:18 +0530

    permutation

---
+++ labs/lab1/permutation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void permute(string str, int l, int r) {
+    if (l == r) {
+        cout << str << endl;
+        return;
+    }
+
+    for (int i = l; i <= r; i++) {
+        swap(str[l], str[i]);      
+        permute(str, l + 1, r);   
+        swap(str[l], str[i]);      
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    permute(str, 0, str.length() - 1);
+    return 0;
+}
