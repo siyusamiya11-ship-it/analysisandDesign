e9fe79e7a01473ec5a067a71b149a414c8941d03
Samiya <siyusamiya11@gmail.com>
2026-01-28 00:08:30 +0530

    velocity

---
+++ labs/lab1/velocity.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate velocity
+int velocity(int u, int a, int t)
+{
+    // Base case
+    if (t == 0)
+        return u;
+
+    // Recursive case
+    return velocity(u, a, t - 1) + a;
+}
+
+int main()
+{
+    int u, a, t;
+
+    cout << "Enter initial velocity: ";
+    cin >> u;
+
+    cout << "Enter acceleration: ";
+    cin >> a;
+
+    cout << "Enter time: ";
+    cin >> t;
+
+    cout << "Final velocity is: " << velocity(u, a, t);
+
+    return 0;
+}
